61ca7305b18ad70411186463652039bb2572812c
Adil Aizhanov <adilfirstus@gmail.com>
2025-03-28 18:40:57 +0500

    d

---
+++ ConsoleApp1/Order.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApp1;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public List<Product> Products { get; set; }
+}
---
+++ ConsoleApp1/Transaction.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApp1;
+
+public class Transaction
+{
+    public int UserId { get; set; }
+    public decimal  Amount  { get; set; }
+}
